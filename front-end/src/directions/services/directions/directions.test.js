a35f0da061dad658556aef60cf7e0e2b7675a717
Arun kumar <arun.redhu05@gmail.com>
2018-07-18 01:04:55 +0530

    test cases added

--- front-end/src/directions/services/directions/directions.test.js
+++ front-end/src/directions/services/directions/directions.test.js
@@ -1,24 +1,5 @@
-// import { restClient } from '../../../../common/services';
-// import { fetchDirections, fetchRoute, fetchToken } from '../directions';
-
-// restClient.get = jest.fn();
-
-// jest.mock('../../../../common/services/rest-client/rest-client');
-
-// describe('Test for directions api', () => {
-//     it('Should test for fetchToken method', async () => {
-//         const token = await fetchToken('from', 'to');
-//         expect(token).toBeDefined();
-//     });
-
-//     it('Should test for fetchToken method', async () => {
-//         const token = await fetchRoute('token');
-//         expect(token).toBeDefined();
-//     });
-// });
-
 import * as directions from '../directions';
-import { restClient } from '../../../../common/services/rest-client/rest-client';
+import { restClient } from '../../../common/services/rest-client/rest-client';
 
 const mockDirectionResponse = {
     status: 'success',
@@ -29,6 +10,10 @@ const mockDirectionResponse = {
     ],
     total_distance: 20000,
     total_time: 1800
+};
+
+const mockTokenResponse = {
+    token: 'token'
 };
 
 describe('Test for directions api', () => {
@@ -43,11 +28,28 @@ describe('Test for directions api', () => {
         expect(token).toBeDefined();
     });
 
-    it('Should test for fetchToken method', async () => {
+    it('Should test for fetchRoute method', async () => {
         const get = jest.spyOn(restClient, 'get');
 
         get.mockImplementation(() =>
             Promise.resolve({ data: mockDirectionResponse })
+        );
+
+        const result = await directions.fetchRoute('token');
+        expect(result).toBeDefined();
+        expect(result.status).toEqual('success');
+    });
+
+    it('Should test for fetchRoute method', async () => {
+        const fetchRoute = jest.spyOn(directions, 'fetchRoute');
+        const fetchToken = jest.spyOn(directions, 'fetchToken');
+
+        fetchRoute.mockImplementation(() =>
+            Promise.resolve({ data: mockDirectionResponse })
+        );
+
+        fetchToken.mockImplementation(() =>
+            Promise.resolve({ data: mockTokenResponse })
         );
 
         const result = await directions.fetchRoute('token');
