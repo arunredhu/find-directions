4a916bb9481ded9813d7363fa7b782c1df729dea
Arun kumar <arun.redhu05@gmail.com>
2018-07-18 11:14:42 +0530

    package json homepage changes

--- front-end/config/webpack.config.prod.js
+++ front-end/config/webpack.config.prod.js
@@ -127,6 +127,13 @@ module.exports = {
                 include: paths.appSrc
             },
             {
+                test: /\.scss$/,
+                loader: ExtractTextPlugin.extract(
+                    'style-loader',
+                    'css?importLoaders=1&-autoprefixer!postcss!scss'
+                )
+            },
+            {
                 // "oneOf" will traverse all following loaders until one will
                 // match the requirements. When no loader matches it will fall
                 // back to the "file" loader at the end of the loader list.
@@ -162,6 +169,62 @@ module.exports = {
                     // tags. If you use code splitting, however, any async bundles will still
                     // use the "style" loader inside the async code so CSS from them won't be
                     // in the main CSS file.
+                    {
+                        test: /\.scss$/,
+                        loader: ExtractTextPlugin.extract(
+                            Object.assign(
+                                {
+                                    fallback: {
+                                        loader: require.resolve('style-loader'),
+                                        options: {
+                                            hmr: false
+                                        }
+                                    },
+                                    use: [
+                                        {
+                                            loader: require.resolve(
+                                                'css-loader'
+                                            ),
+                                            options: {
+                                                importLoaders: 1,
+                                                minimize: true,
+                                                sourceMap: shouldUseSourceMap
+                                            }
+                                        },
+                                        {
+                                            loader: require.resolve(
+                                                'sass-loader'
+                                            )
+                                        },
+                                        {
+                                            loader: require.resolve(
+                                                'postcss-loader'
+                                            ),
+                                            options: {
+                                                // Necessary for external CSS imports to work
+                                                // https://github.com/facebookincubator/create-react-app/issues/2677
+                                                ident: 'postcss',
+                                                plugins: () => [
+                                                    require('postcss-flexbugs-fixes'),
+                                                    autoprefixer({
+                                                        browsers: [
+                                                            '>1%',
+                                                            'last 4 versions',
+                                                            'Firefox ESR',
+                                                            'not ie < 9' // React doesn't support IE8 anyway
+                                                        ],
+                                                        flexbox: 'no-2009'
+                                                    })
+                                                ]
+                                            }
+                                        }
+                                    ]
+                                },
+                                extractTextPluginOptions
+                            )
+                        )
+                        // Note: this won't work without `new ExtractTextPlugin()` in `plugins`.
+                    },
                     {
                         test: /\.css$/,
                         loader: ExtractTextPlugin.extract(
@@ -213,6 +276,7 @@ module.exports = {
                         )
                         // Note: this won't work without `new ExtractTextPlugin()` in `plugins`.
                     },
+
                     // "file" loader makes sure assets end up in the `build` folder.
                     // When you `import` an asset, you get its filename.
                     // This loader doesn't use a "test" so it will catch all modules
@@ -223,7 +287,12 @@ module.exports = {
                         // it's runtime that would otherwise processed through "file" loader.
                         // Also exclude `html` and `json` extensions so they get processed
                         // by webpacks internal loaders.
-                        exclude: [/\.(js|jsx|mjs)$/, /\.html$/, /\.json$/],
+                        exclude: [
+                            /\.(js|jsx|mjs)$/,
+                            /\.html$/,
+                            /\.json$/,
+                            /\.scss$/
+                        ],
                         options: {
                             name: 'static/media/[name].[hash:8].[ext]'
                         }
